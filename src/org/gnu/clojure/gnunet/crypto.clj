139eca816fcb2f012e7f6d43afb80a431b7283f9
David Barksdale <amatus.amongus@gmail.com>
2010-04-12 09:48:15 -0700

    Fix java imports.

--- src/org/gnu/clojure/gnunet/crypto.clj
+++ src/org/gnu/clojure/gnunet/crypto.clj
@@ -1,15 +1,17 @@
-(ns org.gnu.clojure.gnunet.crypto)
+(ns org.gnu.clojure.gnunet.crypto
+  (:import (java.security KeyPairGenerator MessageDigest)
+    java.security.spec.RSAKeyGenParameterSpec))
 
 (defn generate-rsa-keypair
   "Generate a 2048 bit RSA keypair."
   []
-  (let [rsa (java.security.KeyPairGenerator/getInstance "RSA")
-        spec (java.security.spec.RSAKeyGenParameterSpec. 2048 (bigint 257))]
+  (let [rsa (KeyPairGenerator/getInstance "RSA")
+        spec (RSAKeyGenParameterSpec. 2048 (bigint 257))]
     (.initialize rsa spec)
     (.generateKeyPair rsa)))
 
 (defn sha-512
   "Compute the SHA-512 digest of a sequence of bytes."
   [x]
-  (let [sha (java.security.MessageDigest/getInstance "SHA-512")]
+  (let [sha (MessageDigest/getInstance "SHA-512")]
     (.digest sha (byte-array x))))
